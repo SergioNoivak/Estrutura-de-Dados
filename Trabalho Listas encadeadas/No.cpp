59554ecbee633bfb2af9ab901f9e47a350e5b15f
Sergio Souza Novak <serginhosnovak@hotmail.com>
2018-06-13 11:10:55 -0300

    feito 45%

--- Trabalho Listas encadeadas/No.cpp
+++ Trabalho Listas encadeadas/No.cpp
@@ -8,7 +8,6 @@ typedef struct elemento {
 	Lista *prox;
 }Lista;
 
-
 Lista** lista_cria() {
 	Lista** li = (Lista**)malloc(sizeof(Lista*));
 	if (li != NULL)
@@ -35,33 +34,8 @@ int is_vazia(Lista** pLista) {
 	return 0;
 }
 
-//h) Função de inserção de nós no início da lista
-void insere_inicio(Lista** pLista, ) {
-	
-	
-	struct elemento* elemento = (struct elemento*) malloc(sizeof(struct elemento));
 
-	if (elemento == NULL)
-		return;
-	if (pLista == NULL)
-		return;
-
-	elemento->prox = *pLista;
-	*pLista = elemento;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-void entrada_dados_aluno(int matricula, char nome[40], float n1, float  n2, float  n3) {
+Aluno* entrada_dados_aluno(int matricula, char nome[40], float n1, float  n2, float  n3) {
 
 	Aluno* aluno = (Aluno*)malloc(sizeof(Aluno));
 	aluno->matricula = matricula;
@@ -69,11 +43,112 @@ void entrada_dados_aluno(int matricula, char nome[40], float n1, float  n2, floa
 	aluno->n1 = n1;
 	aluno->n2 = n2;
 	aluno->n3 = n3;
+	return aluno;
 }
 
 
+//h) Função de inserção de nós no início da lista
+void insere_inicio(Lista** pLista,Aluno* aluno) {
+	
+	if (pLista == NULL)
+		return;
+	struct elemento* elemento = (struct elemento*) malloc(sizeof(struct elemento));
+	
+	if (elemento == NULL)
+		return;
+
+	elemento->prox = *pLista;
+	*pLista = elemento;
+	elemento->dados.matricula = aluno->matricula;
+	elemento->dados.n1 = aluno->n1;
+	elemento->dados.n2 = aluno->n2;
+	elemento->dados.n3= aluno-> n3;
+	strcpy(elemento->dados.nome, aluno->nome);
+}
 
 
+void insere_fim(Lista** pLista, Aluno* aluno) {
+
+	if (pLista == NULL)
+		return;
+	struct elemento* elemento = (struct elemento*) malloc(sizeof(struct elemento));
+
+	if (elemento == NULL)
+		return;
+
+	struct elemento* i;
+	if (*pLista == NULL) {
+		insere_inicio(pLista, aluno);
+		return;
+	}
+	for (i = *pLista; i->prox != NULL ; i = i->prox)
+		continue;
+
+	i->prox= elemento;
+	elemento->dados.matricula = aluno->matricula;
+	elemento->dados.n1 = aluno->n1;
+	elemento->dados.n2 = aluno->n2;
+	elemento->dados.n3 = aluno->n3;
+	strcpy(elemento->dados.nome, aluno->nome);
+	elemento->prox = NULL;
+}
+
+//j) Função que realiza a inserção de elementos por posição.A função deverá verificar se a posição existe na
+//lista e em caso afirmativo, proceder com a inserção.
+
+
+void insere_posicao(Lista** pLista, Aluno* aluno, int posicao) {
+
+	if (pLista == NULL || posicao < 0)
+		return;
+	
+	struct elemento* elemento = (struct elemento*) malloc(sizeof(struct elemento));
+
+	Lista* i;
+	int contador_posicoes = 0;
+	for (i = *pLista; i != NULL; i = i->prox,contador_posicoes++) {
+		
+		if (contador_posicoes == posicao) {
+
+			Lista* aux = i->prox;
+			i->prox = elemento;
+			elemento->dados.matricula = aluno->matricula;
+			elemento->dados.n1 = aluno->n1;
+			elemento->dados.n2 = aluno->n2;
+			elemento->dados.n3 = aluno->n3;
+			strcpy(elemento->dados.nome, aluno->nome);
+			elemento->prox = aux;
+			return;
+		}
+
+		
+	}
+
+
+
+
+
+}
+
+
+void exibir_aluno(Aluno* aluno) {
+
+	printf("\n{matricula:%d, n1:%f , n2:%f , n3:%f, nome:%s }", aluno->matricula, aluno->n1, aluno->n2, aluno->n3, aluno->nome);
+}
+void exibir_recursivo(Lista* inicial) {
+
+	if (inicial == NULL) {
+
+		printf("NULL");
+
+		return;
+	}
+
+	exibir_aluno(&(inicial->dados));
+	printf("->");
+	exibir_recursivo(inicial->prox);
+
+}
 
 
 
--- Trabalho Listas encadeadas/No.h
+++ Trabalho Listas encadeadas/No.h
@@ -11,7 +11,17 @@ typedef struct elemento Lista;
 
 Lista** lista_cria();
 int lista_libera(Lista** li);
+void insere_inicio(Lista** pLista, struct elemento* elemento);
+int is_vazia(Lista** pLista);
 
+//b) Verificação se a lista está vazia.
+int is_vazia(Lista** pLista);
+Aluno*  entrada_dados_aluno(int matricula, char nome[40], float n1, float  n2, float  n3);
+//h) Função de inserção de nós no início da lista
+void insere_inicio(Lista** pLista, Aluno* aluno);
+void exibir_aluno(Aluno* aluno);
+void exibir_recursivo(Lista* inicial);
+void insere_posicao(Lista** pLista, Aluno* aluno, int posicao);
 
 
 //Funcoes auxiliares
